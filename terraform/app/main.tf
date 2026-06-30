57431b7308f8674428a9612b49a78d62d3b58348
Enes <enes.gashi001@gmail.com>
2026-06-30 20:22:24 +0200

    Add HTTP Security Group Rule

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -95,6 +95,14 @@ resource "aws_security_group" "flask_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   ingress {
     from_port   = 22
     to_port     = 22
