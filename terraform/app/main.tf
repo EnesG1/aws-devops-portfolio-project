4b422fa6f6530c40783702cffc60f71aa054f1b3
Enes <enes.gashi001@gmail.com>
2026-06-30 20:10:26 +0200

    Add IAM role for EC2-ECR connection

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -47,17 +47,43 @@ resource "aws_instance" "server_app" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
-  key_name = "Mykey"
+  key_name = "aws-key"
 
   vpc_security_group_ids = [
     aws_security_group.flask_app.id
   ]
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   tags = {
     Name = "flask-devops-server"
   }
 }
 
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_access" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-ecr-profile"
+  role = aws_iam_role.ec2_role.name
+}
 
 resource "aws_security_group" "flask_app" {
   name = "flask-app-sg"
