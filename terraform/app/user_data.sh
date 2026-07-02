f550efdb6dafff5e39f096c3940d97e8ca582f4d
Enes <enes.gashi001@gmail.com>
2026-07-02 20:14:37 +0200

    Finish touches on bootstrap

--- terraform/app/user_data.sh
+++ terraform/app/user_data.sh
@@ -11,6 +11,7 @@ apt install -y docker.io curl unzip
 systemctl enable docker
 systemctl start docker
 
+usermod -aG docker ubuntu
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
